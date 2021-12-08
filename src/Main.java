1ed5513c0f63c83b2e81b0aa0871c9480b3c4d4d
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2021-12-08 09:00:54 +0500

    Added .gitignore, added comments to Main.java

--- src/Main.java
+++ src/Main.java
@@ -32,10 +32,19 @@ public class Main {
         System.out.println("***********************");
     }
 
+    /*
+    Метод, принимающий на вход два целых числа и проверяющий,
+    что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true,
+    в противном случае – false.
+    */
     public static boolean checkSum(int a, int b) {
         return a + b > 10 && a + b < 20;
     }
 
+    /*
+    Метод, которому в качестве параметра передается целое число,
+    метод должен напечатать в консоль, положительное ли число передали или отрицательное.
+    */
     public static void checkPositiveOrNegative(int number) {
         if (number < 0) {
             System.out.println(number +" - отрицательное число!");
@@ -44,16 +53,28 @@ public class Main {
         }
     }
 
+    /*
+    Метод, которому в качестве параметра передается целое число.
+    Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+    */
     public static boolean checkNegativeNumber(int number) {
         return number < 0;
     }
 
+    /*
+    Метод, которому в качестве аргументов передается строка и число,
+    метод должен отпечатать в консоль указанную строку, указанное количество раз.
+    */
     public static void multiplePrintText(String text, int number) {
         for (int i = 0; i < number; i++) {
             System.out.println(text);
         }
     }
 
+    /*
+    Метод, который определяет, является ли год високосным,
+    и возвращает boolean (високосный - true, не високосный - false).
+    */
     public static boolean checkLeapYear(int year) {
         return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
